4c65536ba563d69a8b3ee3a22595a110732e38cc
ishand1993 <79570930+ishand1993@users.noreply.github.com>
2022-11-08 00:15:52 -0500

    Updates

--- src/components/Expenses/js/Expenses.js
+++ src/components/Expenses/js/Expenses.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../../UI/js/Card";
 import "../css/Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -28,6 +28,6 @@ function Expenses(props) {
       ></ExpenseItem>
     </Card>
   );
-}
+};
 
 export default Expenses;
